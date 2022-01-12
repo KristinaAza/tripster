fbca0244c5d97d2c7a32fd7719d33862f2f01f53
Kristina Aza <kristina.aza@gmail.com>
2022-01-12 23:28:48 +0000

    Updated server, tempates

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+from model import connect_to_db
+import crud
 
 app = Flask(__name__)
 
-@app.route("/")
-def render_homepage():
+@app.route("/trips")
+def render_trips():
 
-    return render_template("homepage.html")
+    trips = crud.get_all_trips(user_id=1)
+
+    return render_template("trips.html", trips=trips)
 
 
 if __name__ == "__main__":
 
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
