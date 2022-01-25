086d204ef79216518da5d62cf9912b584effd772
Kristina Aza <kristina.aza@gmail.com>
2022-01-25 19:37:51 +0000

    Added hiding checked trip items

--- static/js/categories.js
+++ static/js/categories.js
@@ -27,7 +27,7 @@ document.querySelector("#add-category").addEventListener("submit", evt => {
         categoryElement.innerHTML= `<a id="link-${categoryAdded.id}" href="/categories/${categoryAdded.id}">${categoryAdded.name}</a>
                                     <input id="id-field-${categoryAdded.id}" class="form-control" value="${categoryAdded.id}" type="hidden" name="id">
                                     <button id="open-edit-form-${categoryAdded.id}" class="edit-button" onclick="">Edit</button>
-                                    <form id="form-${categoryAdded.id}" class="edit-category" style="visibility:hidden;" method="POST" action="/category">
+                                    <form id="form-${categoryAdded.id}" class="edit-category" style="display:none;" method="POST" action="/category">
                                         <label for="name-field">Name</label>
                                         <input id="name-${categoryAdded.id}-field" class="form-control" type="text" name="name" value="${categoryAdded.id}">
                                         <input type="submit" value="Save" class="btn btn-primary">
--- static/js/trip.js
+++ static/js/trip.js
@@ -1,5 +1,5 @@
 function openAddForm(element) {
-    element.parentElement.querySelector('.add-item').setAttribute("style", "visibility:visible;");
+    element.parentElement.querySelector('.add-item').setAttribute("style", "display:block;");
 }
 
 function changeChecked(element, id) {
@@ -16,23 +16,26 @@ function changeChecked(element, id) {
     .then(responseJson => {
         checkedStatus = responseJson.checkedStatus;
         const parent = element.parentElement;
-        const style = checkedStatus ? "text-decoration:line-through;" : "text-decoration:none;";
+        const style = checkedStatus ? "text-decoration:line-through; display:inline;" : "text-decoration:none; display:inline;";
         parent.querySelector('span').setAttribute("style", style);
+        
+        if (style === "text-decoration:line-through; display:inline;") {
+            parent.querySelector('span').classList.add('hide');
+            element.classList.add('hide');
+        } else {
+            parent.querySelector('span').classList.remove('hide');
+            element.classList.remove('hide');
+        }
+        console.log("changeChecked is done running")
     })
 }
 
+function toggle(button) {
+    const text = button.innerText;
+    button.innerText = text === "Show All Items" ? "Hide checked out items" : "Show All Items";
 
-// function toggleStrikethrough(element, id) {
-//      const checkedStatus = changeChecked(id);
-
-//      const parent = element.parentElement;
-//      const style = checkedStatus ? "text-decoration:line-through;" : "text-decoration:none;";
-//      console.log(checkedStatus);
-//      console.log(style);
-    
-//      // const parentStyle = parent.getAttribute('style');
-//      // const style = (parentStyle === "text-decoration:line-through;") ? "text-decoration:none;" : "text-decoration:line-through;";
-//      parent.setAttribute("style", style);
-
-// }
-
+    const items_to_hide = document.querySelectorAll(".hide");
+    for (const item of items_to_hide) {
+        item.style.display = (item.style.display === "inline") ? "none" : "inline";
+    }
+}
